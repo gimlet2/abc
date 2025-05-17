a63d1cfd5bd3691deb1e084a81ed5ff737d3fc5e
Andrei Chernyshev <andrey.chernishov@gmail.com>
2025-05-17 10:44:40 +0200

    refactor menu

---
+++ stories.js
@@ -0,0 +1,26 @@
+const stories = {
+    "DE": [
+        "Der kleine Bär und der Apfel\n" +
+        "\n" +
+        "Der kleine Bär heißt Ben.\n" +
+        "Ben lebt im Wald.\n" +
+        "\n" +
+        "Eines Tages hat Ben Hunger.\n" +
+        "Er sagt: „Ich will einen Apfel!“\n" +
+        "\n" +
+        "Ben läuft zum Apfelbaum.\n" +
+        "Aber – oh nein! Die Äpfel sind zu hoch.\n" +
+        "\n" +
+        "Da kommt sein Freund Max, die Maus.\n" +
+        "Max sagt: „Ich helfe dir!“\n" +
+        "\n" +
+        "Max klettert auf den Baum.\n" +
+        "Plopp! Ein Apfel fällt runter.\n" +
+        "\n" +
+        "Ben freut sich: „Danke, Max!“\n" +
+        "Sie essen den Apfel zusammen.\n" +
+        "\n" +
+        "Mmh, lecker!\n" +
+        "Ben sagt: „Freunde sind toll!“"
+    ]
+};
